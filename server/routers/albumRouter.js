25d9aa18d6a76b818e5605802bbdd3964cead25f
nqdat2002 <datfeed.2706@gmail.com>
2023-05-20 17:41:20 +0700

    Update albumRouter.js

--- server/routers/albumRouter.js
+++ server/routers/albumRouter.js
@@ -8,6 +8,7 @@ router.get("/getAll", albumController.getAll);
 router.post("/save", albumController.save);
 router.get("/getOne/:id", albumController.getOne);
 router.put("/update/:id", albumController.update);
+
 router.delete("/delete/:id", albumController.delete);
 
 module.exports = router;
